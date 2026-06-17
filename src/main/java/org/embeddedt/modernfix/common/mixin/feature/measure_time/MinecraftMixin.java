e2716cb0bb75b224b736f3626b7c6c64e92ac9d8
coredex-source <samdhi2323@gmail.com>
2026-06-17 17:22:57 +0530

    5.27.17 - commit.2
    - Patch for 26.2 again.

--- src/main/java/org/embeddedt/modernfix/common/mixin/feature/measure_time/MinecraftMixin.java
+++ src/main/java/org/embeddedt/modernfix/common/mixin/feature/measure_time/MinecraftMixin.java
@@ -1,10 +1,10 @@
 package org.embeddedt.modernfix.common.mixin.feature.measure_time;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.screens.Overlay;
+import net.minecraft.client.gui.Gui;
 import org.embeddedt.modernfix.ModernFixClient;
 import org.embeddedt.modernfix.annotation.ClientOnlyMixin;
-import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,11 +16,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @ClientOnlyMixin
 public class MinecraftMixin {
     // TODO re-add datapack reload time measurement
-    @Shadow @Nullable public Overlay overlay;
+    @Shadow @Final public Gui gui;
 
     @Inject(method = "tick", at = @At("HEAD"))
     private void onClientTick(CallbackInfo ci) {
-        if(this.overlay == null && ModernFixClient.INSTANCE != null) {
+        if(this.gui.screen() == null && ModernFixClient.INSTANCE != null) {
             ModernFixClient.INSTANCE.onGameLaunchFinish();
         }
     }
